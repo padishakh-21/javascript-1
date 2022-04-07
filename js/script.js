0ab1a542b2a69d415f9348277fb9a63059622d56
email <eraliyevjamshid65@gmail.com>
2022-04-07 20:41:41 +0500

    git init .

---
+++ js/script.js
@@ -0,0 +1,14 @@
+alert("Xush kelibsiz!");
+var questionName = prompt("Ismingiz nima?", "Jamshid");
+
+var lastName = prompt("Familiyangiz nima?", "Eraliyev");
+
+var age = prompt("Tug'ilgan yilingiz", "2003");
+
+var questionRule = prompt("Qayerda ishlaysiz");
+
+console.log("Ismingiz nima? " + "Jamshid");
+
+console.log("Familiyangiz nima? " + "Eraliyev");
+
+console.log("Tug'ilgan yilingiz? " + "2003");
