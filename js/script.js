495ab08ca2bec8c21f1024aa6d72520fb85c27a7
email <eraliyevjamshid65@gmail.com>
2022-04-11 15:21:53 +0500

    add js elements

--- js/script.js
+++ js/script.js
@@ -7,8 +7,8 @@ var age = prompt("Tug'ilgan yilingiz", "2003");
 
 var questionRule = prompt("Qayerda ishlaysiz");
 
-console.log("Ismingiz nima? ");
+console.log("Ismingiz nima?");
 
-console.log("Familiyangiz nima? ");
+console.log("Familiyangiz nima?");
 
-console.log("Tug'ilgan yilingiz? ");
+console.log("Tug'ilgan yilingiz?");
